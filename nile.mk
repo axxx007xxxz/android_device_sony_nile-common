606a2588a203f37bf57a2f796037ac9ffaa8fee3
axxx007xxxz <mattiabrsrn7@gmail.com>
2020-04-19 22:04:35 +0200

    nile-common: Set overlays the CarbonROM way
    CarbonROM doesn't use LineageSDK so overlays have to be set in "frameworks/base".

--- nile.mk
+++ nile.mk
@@ -23,7 +23,7 @@ $(call inherit-product, vendor/sony/nile-common/nile-common-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-carbon
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay-lineage/lineage-sdk
